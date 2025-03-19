91a27d2e67f0cf6e18125c579086b653cbabd3b2
HaganMc <hagan_mc@outlook.com>
2025-03-19 15:21:29 -0500

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "password_resets/new"
+  get "password_resets/edit"
   root "pages#home"
   get "/help", to: "pages#help"
   get "/about", to: "pages#about"
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   resources :users
   get "/test_log_out", to: "test#test_log_out"
   resources :account_activations, only: [ :edit ]
+  resources :password_resets, only: [ :new, :create, :edit, :update ]
 end
