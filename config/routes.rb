4218031e3a6cdb96f656bda2a85303c18b347ec5
Hagan McBride <haganmcbride@Hagans-MacBook-Air.local>
2025-03-25 11:13:54 -0500

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "password_resets/new"
-  get "password_resets/edit"
   root "pages#home"
   get "/help", to: "pages#help"
   get "/about", to: "pages#about"
@@ -9,10 +7,14 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
-  get "/test_log_out", to: "test#test_log_out"
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [ :edit ]
   resources :password_resets, only: [ :new, :create, :edit, :update ]
   resources :microposts, only: [ :create, :destroy ]
+  resources :relationships, only: [ :create, :destroy ]
   get "/microposts", to: "pages#home"
 end
